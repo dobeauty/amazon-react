0cbb1d7123a1ee76e46f290b6ef2c957bd4f8542
동근 <macbookpro@Macbookui-MacBookPro.local>
2022-09-07 00:03:32 +0900

    amazon-clone-1day

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyCzqURw1FgcKAZyCR-NYF6YuA5xoBcPF3A",
+    authDomain: "clone-47cc8.firebaseapp.com",
+    projectId: "clone-47cc8",
+    storageBucket: "clone-47cc8.appspot.com",
+    messagingSenderId: "982609670462",
+    appId: "1:982609670462:web:5b06a1cbd3b20cfd407c6f",
+    measurementId: "G-L94K3BTRD6"
+  };
